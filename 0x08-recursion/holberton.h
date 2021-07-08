0a4e1814c90b330feceef8c73946179b0438ef53
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-07-08 06:51:29 -0700

    Read me and Holberton file

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*_putchar prototype*/
+int _putchar(char c);
+
+/*Task prototypes*/
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int check_prime(int x, int y);
+int palindrome_check_a(char *s);
+int palindrome_check_b(char *s);
+int is_palindrome(char *s);
+#endif
