bf024bed8b0226862a4e9b20cd39b5b96dc12ae9
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-07-16 07:24:57 -0700

    Updating Git

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+
+/*Prototypes*/
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+
+#endif
