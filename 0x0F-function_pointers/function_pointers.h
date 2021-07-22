e6202e50bb74a04a21b9d11a876d8fcc73fff18c
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-07-22 07:02:24 -0700

    Tasks 0 & 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,19 +7,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
-/*_putchar prototype*/
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 #endif
